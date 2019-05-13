823fec25a3658151ead954247590ef95d0642b25
leannr78 <leann.roberts@gmail.com>
2019-05-13 01:28:24 -0400

    Updated Portfolio
    Added Projects

---
+++ assets/javascript/script.js
@@ -0,0 +1,8 @@
+
+'use strict';
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        anchors: ['home', 'portfolio', 'about', 'contact'],
+        menu: '#navigation'
+    });
+});
